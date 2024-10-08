e488508b943c6552aac945d2965a2e433ab8d59e
Jason Livesay <runvnc@gmail.com>
2024-10-08 05:18:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ah_openai
+
+A module for OpenAI integration.
+
+## Installation
+
+You can install this package using pip:
+
+```
+pip install ah_openai
+```
+
+## Usage
+
+Describe how to use your module here.
---
+++ src/ah_openai/mod.py
@@ -0,0 +1,62 @@
+from lib.providers.services import service
+import os
+import openai
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+ 
+client = openai.AsyncOpenAI()
+
+@service()
+async def stream_chat(model, messages=[], context=None, num_ctx=2048, temperature=0.0, max_tokens=3724, num_gpu_layers=12):
+    print("stream_chat called")
+    print("model", model)
+    try:
+        if model == 'o1-preview' or model == 'o1-mini':
+            content = await sync_chat_o1(model, messages)
+
+            async def content_stream_():
+                yield content
+
+            return content_stream_()
+
+        if not model or model == '':
+            model = 'chatgpt-4o-latest'
+        stream = await client.chat.completions.create(
+            model="chatgpt-4o-latest",
+            #model="o1-preview",
+            stream=True,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens
+        )
+
+        async def content_stream(original_stream):
+            async for chunk in original_stream:
+                if os.environ.get('AH_DEBUG') == 'True':
+                    print('\033[92m' + str(chunk.choices[0].delta.content) + '\033[0m', end='')
+
+                yield chunk.choices[0].delta.content or ""
+
+        return content_stream(stream)
+
+    except Exception as e:
+        print('openai error:', e)
+
+
+from openai import OpenAI
+sync_client = OpenAI()
+
+async def sync_chat_o1(model, messages):
+    messages_copy = messages.copy()
+    messages_copy[0]['role'] = 'user'
+    print("calling ", model)
+    print("messages_copy", messages_copy)
+    response = sync_client.chat.completions.create(
+        model = model,
+        messages = messages_copy
+    )
+    print("response from o1-preview received:")
+    response = response.choices[0].message.content
+    print(response)
+    return response
+
