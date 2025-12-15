6aa8fdf5dc66223748afa276e7ab1c5f464ba772
Jason Livesay <runvnc@gmail.com>
2025-12-15 12:27:57 -0600

    reasoning effort none

--- src/ah_openai/mod.py
+++ src/ah_openai/mod.py
@@ -136,12 +136,13 @@ async def stream_chat(model, messages=[], context=None, num_ctx=200000,
         elif model_name.startswith('gpt-5.2'):
             messages[0]['role'] = "developer"
             response_format = { "type": "text"}
-            if reasoning_effort == "off":
-                reasoning_effort = "none"
- 
+
             #max_tokens = 20000
             temperature = 1
             reasoning_effort = context.agent.get('thinking_level')
+            if reasoning_effort == "off":
+                reasoning_effort = "none"
+ 
         elif model_name.startswith('gpt-5'):
             messages[0]['role'] = "developer"
             response_format = { "type": "text"}
